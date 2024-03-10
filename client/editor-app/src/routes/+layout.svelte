53f91fd259cb433383efdf8f4c5d04fdd8cdcba7
cblack389 <negruclaudiu389@yahoo.com>
2024-03-10 16:37:51 +0200

    Add some css

--- client/editor-app/src/routes/+layout.svelte
+++ client/editor-app/src/routes/+layout.svelte
@@ -4,4 +4,26 @@
 	<a href="/documents">documents</a>
 </nav>
 
+<style>
+	nav {
+		background-color: rgb(31, 4, 56);
+		display: flex; 
+        justify-content: center;
+        align-items: center;
+		font-size: x-large;
+		color: aliceblue;
+		border-radius: 50;
+		
+	}
+
+	a {
+		margin-left: 5%;
+	}
+
+	:global(body) {
+		overflow: hidden;
+		background-color: rgb(45, 45, 87);
+	}
+</style>
+
 <slot />
--- client/editor-app/src/routes/edit/+page.svelte
+++ client/editor-app/src/routes/edit/+page.svelte
@@ -1,8 +1,21 @@
-<textarea/>
+<main>
+    <textarea/>
+</main>
+
 
 <style>
+    main {
+        width:100%;
+        height:100%;
+    }
     textarea {
         width: 400px;
         height: 400px;
+        background-color: black;
+        color: white;
+        top: 50%;
+        left: 50%;
+        margin-left: 35%;
+        margin-top: 5%;
     }
 </style>
