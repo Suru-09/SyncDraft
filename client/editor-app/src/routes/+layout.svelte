bc7b52f6c215cf6a4d6c4c330cc57f03c5cdaa98
cblack389 <negruclaudiu389@yahoo.com>
2024-03-10 15:39:58 +0200

    Add basic svelte project

---
+++ client/editor-app/src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<nav>
+    <a href="/">home</a>
+	<a href="/edit">editor</a>
+	<a href="/documents">documents</a>
+</nav>
+
+<slot />
---
+++ client/editor-app/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<main>
+    <h1>SyncDraft</h1>
+    <a href="/edit">
+        <button>New Draft</button>
+    </a>
+</main>
+
+
+<style>
+    main {
+        display: block; /* or grid */
+        justify-content: center;
+        align-items: center;
+    }
+    h1 {
+        color:cornflowerblue;
+        font-family: 'Courier New', Courier, monospace;
+        font-size: xx-large;
+    }
+</style>
