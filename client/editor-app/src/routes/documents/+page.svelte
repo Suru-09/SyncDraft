cd0e0e9f32743b22423819522988cde50dd28d9a
Surugiu Dragos <dragos.surugiu13@yahoo.com>
2024-04-27 22:56:26 +0300

    Made a new create doc button and page size selection

--- client/editor-app/src/routes/documents/+page.svelte
+++ client/editor-app/src/routes/documents/+page.svelte
@@ -2,13 +2,21 @@
     import { onMount } from 'svelte';
     import { backendUrl } from '../../config';
     import { Table, TableBody, TableBodyCell, TableBodyRow,
-         TableHead, TableHeadCell, Pagination 
+         TableHead, TableHeadCell, Pagination, 
+		 Button, Select, Label 
     } from 'flowbite-svelte';
     import axios from 'axios';
     import {TrashBinOutline, EditOutline, ArrowLeftOutline, ArrowRightOutline } from 'flowbite-svelte-icons';
     import { userDocuments, loggedUser } from '../../stores';
+    let selected = 10;
+    let pageSizes = [
+        { value: 10, name: '10' },
+        { value: 25, name: '25' },
+        { value: 50, name: '50' },
+        { value: 100, name: '100' }
+    ];
 
-    let pageSize = 10;
+    let pageSize = selected;
     let currentDocsBodySize = 40;
     let helper = { start: 1, end: pageSize, total: 100 };
     const previous = () => {
@@ -35,7 +43,13 @@
         });
     }
 
-    let currentDocs = [];
+    let currentDocs = $userDocuments;
+
+    const handleSelectPageSize = async () => {
+        helper.end = Math.min(selected, helper.total);;
+        pageSize = selected;
+        currentDocs = $userDocuments.slice(helper.start - 1, helper.end);
+    }
 
     async function fetchDocuments() {
         try {
@@ -43,13 +57,9 @@
                 doc_owner: $loggedUser.username,
             };
             const response = await axios.get(`${backendUrl}/doc/get`, {params});
-            console.log(response);
             $userDocuments = response.data;
-            console.log($userDocuments);
             currentDocs = $userDocuments.slice(helper.start - 1, helper.end);
-            console.log(currentDocs.body);
             trimDocsBody();
-            console.log(currentDocs.body);
             helper.total = $userDocuments.length;
             helper.end = Math.min(helper.total, helper.end);
         } catch (error) {
@@ -65,52 +75,68 @@
 </script>
 
 <main class="bg-white dark:bg-gray-800">
-    <Table style="width: 95%; margin-top: 3rem; margin-left: auto; margin-right: auto;" striped={true}>
-        <TableHead>
-            <TableHeadCell>Document Name</TableHeadCell>
-            <TableHeadCell>Document Owner</TableHeadCell>
-            <TableHeadCell>Body</TableHeadCell>
-            <TableHeadCell>Edit</TableHeadCell>
-            <TableHeadCell>Delete</TableHeadCell>
-        </TableHead>
-        <TableBody tableBodyClass="divide-y">
-            {#each currentDocs as document}
-                <TableBodyRow>
-                    <TableBodyCell>{document.doc_name}</TableBodyCell>
-                    <TableBodyCell>{document.doc_owner}</TableBodyCell>
-                    <TableBodyCell>{document.body}</TableBodyCell>
-                    <TableBodyCell>
-                        <EditOutline class="w-6 h-5 me-2 text-primary-500 hover:outline dark:text-primary-500" />
-                    </TableBodyCell>
-                    <TableBodyCell>
-                        <TrashBinOutline class="w-6 h-5 me-2 text-primary-500 hover:outline dark:text-primary-500" />
-                    </TableBodyCell>
-                </TableBodyRow>
-            {/each}
-        </TableBody>
-    </Table>
-    
-    <div class="flex flex-col items-center justify-center gap-2 mt-5">
-      <div class="text-sm text-gray-700 dark:text-gray-400">
-        Showing <span class="font-semibold text-gray-900 dark:text-white">{helper.start}</span>
-        to
-        <span class="font-semibold text-gray-900 dark:text-white">{helper.end}</span>
-        of
-        <span class="font-semibold text-gray-900 dark:text-white">{helper.total}</span>
-        Entries
-    </div>
-    
-    <Pagination large>
-        <button type="button" slot="prev" on:click={previous} class="flex items-center gap-2 dark:text-white text-gray-900 bg-white dark:bg-gray-800">
-            <ArrowLeftOutline class="w-3.5 h-3.5 me-2"/>
-            Prev
-        </button>
-        <button type="button" slot="next" on:click={next} class="flex items-center gap-2 dark:text-white text-gray-900 bg-white dark:bg-gray-800">
-            Next
-            <ArrowRightOutline class="w-6 h-6 me-2"/>
-        </button>
-    </Pagination>
+        <Table divClass="w-3/4 flex flex-col m-auto" striped={true}>
+            <TableHead>
+                <TableHeadCell>Document Name</TableHeadCell>
+                <TableHeadCell>Document Owner</TableHeadCell>
+                <TableHeadCell>Body</TableHeadCell>
+                <TableHeadCell>Edit</TableHeadCell>
+                <TableHeadCell>Delete</TableHeadCell>
+            </TableHead>
+            <TableBody tableBodyClass="divide-y">
+                {#each currentDocs as document}
+                    <TableBodyRow>
+                        <TableBodyCell>{document.doc_name}</TableBodyCell>
+                        <TableBodyCell>{document.doc_owner}</TableBodyCell>
+                        <TableBodyCell>{document.body}</TableBodyCell>
+                        <TableBodyCell>
+                            <EditOutline class="w-6 h-5 me-2 text-primary-500 hover:outline dark:text-primary-500" />
+                        </TableBodyCell>
+                        <TableBodyCell>
+                            <TrashBinOutline class="w-6 h-5 me-2 text-primary-500 hover:outline dark:text-primary-500" />
+                        </TableBodyCell>
+                    </TableBodyRow>
+                {/each}
+            </TableBody>
+            <div class="under-table">
+                    <div class="flex flex-col items-center justify-center gap-2 mt-5">
+                        <div class="text-sm text-gray-700 dark:text-gray-400">
+                            Showing <span class="font-semibold text-gray-900 dark:text-white">{helper.start}</span>
+                            to
+                            <span class="font-semibold text-gray-900 dark:text-white">{helper.end}</span>
+                            docs of
+                            <span class="font-semibold text-gray-900 dark:text-white">{helper.total}</span>
+                            total docs
+                        </div>
+                    
+                        <Pagination large>
+                            <button type="button" slot="prev" on:click={previous} class="flex items-center gap-2 dark:text-white text-gray-900 bg-white dark:bg-gray-800">
+                                <ArrowLeftOutline class="w-3.5 h-3.5 me-2"/>
+                                Prev
+                            </button>
+                            <button type="button" slot="next" on:click={next} class="flex items-center gap-2 dark:text-white text-gray-900 bg-white dark:bg-gray-800">
+                                Next
+                                <ArrowRightOutline class="w-6 h-6 me-2"/>
+                            </button>
+                        </Pagination>
+                    </div>
+                    <Label>
+                        Select page size
+                        <Select class="mt-2" items={pageSizes} bind:value={selected} on:change={handleSelectPageSize}/>
+                    </Label>
+                    <Button> Create a new document </Button>
+            </div>
+        </Table>
 </main>
 
-<style>
+<style>  
+    .under-table {
+        min-width: 100%;
+        width: 215%;
+        overflow: auto;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+    }
 </style>
